69c1e627887bfafe907436e6603d0faea4e475d2
Ryan Kang <rkang626@gmail.com>
2021-01-08 22:57:21 +0000

    add code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  } else if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN);
+  } else {
+    return "invalid inputs";
+  }
 }
 
 module.exports = sum;
